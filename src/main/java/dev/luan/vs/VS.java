3c1585db0160cb1fb27e66a9eb0535b73876d0cd
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-18 17:37:14 +0200

    Add VS Java plugin base class
    Introduce the base class for the VS plugin, setting up initial structure and configuration. This includes the setup of onEnable and onDisable methods, and initializes the ConfigManager. The class uses Lombok annotations and extends JavaPlugin.

---
+++ src/main/java/dev/luan/vs/VS.java
@@ -0,0 +1,21 @@
+package dev.luan.vs;
+
+import dev.luan.vs.config.ConfigManager;
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public final class VS extends JavaPlugin {
+
+    private ConfigManager configManager;
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
